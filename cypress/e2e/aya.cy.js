05e5697db1b83d31e6dfb21449c687eb60350b25
Yevheniia Sudnitsyna <ysudnitsyna@macbook-air.lan>
2022-09-18 14:03:36 -0500

    adding new actions

--- cypress/e2e/aya.cy.js
+++ cypress/e2e/aya.cy.js
@@ -5,16 +5,13 @@ describe("test for aya", () => {
         cy.intercept('GET', '**AyaHealthcareWeb/job/search**').as('jobsItems2')
         cy.visit('/travel-nursing/travel-nursing-jobs')
 
+        cy.location('pathname').then((loc => {
+            cy.log('the URL is:', loc)
+        }))
         cy.wait('@jobsItems', { timeout: 60000 }).then((response) => {
             const listOfJobs = response.response.body.count
             cy.log("count of items is:", listOfJobs)
            
-        })
-        cy.reload()
-        cy.wait('@jobsItems2', { timeout: 60000 }).then((response) => {
-            const listOfJobs = response.response.body.count
-            cy.log("count of items is:", listOfJobs)
-            
         })
     })
 })
